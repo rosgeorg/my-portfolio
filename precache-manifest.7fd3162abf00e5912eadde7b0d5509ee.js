8990b45cfe219140aee33139a925cbd0bd088a20
rosgeorg <js.2701e@gmail.com>
2025-11-05 14:30:23 -0600

    Updates

---
+++ precache-manifest.7fd3162abf00e5912eadde7b0d5509ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce26e19d963fadfc700a7745b1ff4dd1",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "e6028bdba6677c22c392",
+    "url": "/my-portfolio/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b97743b1076a4f90e2bf",
+    "url": "/my-portfolio/static/js/2.8f18329a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-portfolio/static/js/2.8f18329a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6028bdba6677c22c392",
+    "url": "/my-portfolio/static/js/main.1da98f6c.chunk.js"
+  },
+  {
+    "revision": "5d4511f3fcec99c33304",
+    "url": "/my-portfolio/static/js/runtime-main.c58b4907.js"
+  }
+]);
